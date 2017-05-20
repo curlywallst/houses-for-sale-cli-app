83d4ac77c7232f4361fceee36d075cb4673581ae
curlywallst <nnoyes@comcast.net>
2017-05-20 17:49:16 -0400

    Started working on the scraping

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,6 +3,37 @@ require_relative "../lib/house.rb"
 require 'nokogiri'
 require 'colorize'
 
+
+
 class CommandLineInteface
+
+attr_accessor :town, :state
+BASE_PATH = "http://www.realtor.com/realestateandhomes-search/"
+
+  def initialize
+    puts "your town?:"
+    town=gets.strip
+    puts "your state?:"
+    state = gets.strip
+    location_key = "#{town}_#{state}"
+    @index_url = BASE_PATH+location_key
+  end
+
+  def run
+    self.get_houses
+
+    display_listings
+  end
+
+  def get_houses
+    local_page = Scraper.get_page(@index_url)
+    listings = Scraper.get_listings(local_page)
+  end
+
+  def display_listings
+
+  end
+
+
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,16 @@ require 'pry'
 
 class Scraper
 
+
+  def self.get_page(index_url)
+    @doc = Nokogiri::HTML(open(index_url))
+  end
+
+  def self.get_listings(local_page)
+    listings = @doc.css("div.js-record-user-activity.js-navigate-to.srp-item div.aspect-content div.srp-item-details div.srp-item-address span").text
+    # listings = listings.css("div.srp_item-details").text
+    binding.pry
+  end
+
+
 end
